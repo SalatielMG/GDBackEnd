6df5af23cb5f768ae728e48452e7fcf57ac310ac
SalatielMG <salatiel.monteroglez@gmail.com>
2019-10-19 10:30:49 -0500

    Avance movements v4

--- app/control/ControlMovement.php
+++ app/control/ControlMovement.php
@@ -6,38 +6,66 @@
  * Time: 14:43
  */
 require_once (APP_PATH."model/Movement.php");
+require_once ("ControlAccount.php");
 
 class ControlMovement extends Valida
 {
     private $m;
+    private $ctrlAccount;
     private $pagina = 0;
+    private $pk_Movement = array();
+
+    private $where = "";
+    private $select = "";
+    private $table = "";
+
     public function __construct()
     {
         $this -> m = new Movement();
     }
-    public function buscarMovementsBackup() {
-        $idBackup = Form::getValue('idBack');
-        $select = $this -> m -> mostrar("1 ORDER BY CC.date_record",
+    public function buscarMovementsBackup($isQuery = true) {
+        if ($isQuery) {
+            $this -> pk_Movement["id_backup"] = Form::getValue('id_backup');
+            $this -> pagina = Form::getValue("pagina");
+            $this -> pagina = $this -> pagina * $this -> limit;
+        }
+        $arreglo = array();
+        $exixstIndexUnique = $this -> m -> verifyIfExistsIndexUnique($this -> m -> nameTable);
+
+        if ($exixstIndexUnique["indice"]) {
+
+        } else {
+            $this -> select = "bm.*, (SELECT symbolCurrency(" . $this -> pk_Movement["id_backup"] . ", '', bm.id_account)) AS symbol, (SELECT nameAccount(" . $this -> pk_Movement["id_backup"] . ", bm.id_account)) AS nameAccount, (SELECT nameCategory(" . $this -> pk_Movement["id_backup"] . ", bm.id_category)) as nameCategory,  COUNT(bm.id_backup) repeated";
+            $this -> table = "backup_movements bm";
+            $this -> where = (($isQuery) ? "bm.id_backup = " . $this -> pk_Movement["id_backup"] : $this -> conditionVerifyExistsUniqueIndex($this -> pk_Movement, $this -> m -> columnsTableIndexUnique, false, "bm.")) . " GROUP BY " . $this -> namesColumns($this -> m -> columnsTableIndexUnique, "bm.") . " HAVING COUNT( * ) >= 1 ORDER BY bm.date_idx " . (($isQuery) ? "limit $this->pagina,$this->limit" : "");
+        }
+        /*$select = $this -> m -> mostrar("1 ORDER BY CC.date_record",
             "CC.*",
             "(SELECT bm.*, bc.symbol, bac.name as account, bcat.name as category FROM backup_movements bm, backup_currencies bc, backup_accounts bac, backup_categories bcat WHERE bm.id_backup = bc.id_backup AND bm.id_backup = bac.id_backup AND bm.id_account = bac.id_account AND bm.id_backup = bcat.id_backup AND (bm.id_category = bcat.id_category) AND bm.id_backup = $idBackup
                     UNION
                    SELECT bm.*, bc.symbol, bac.name as account, '' AS category FROM backup_movements bm, backup_currencies bc, backup_accounts bac WHERE bm.id_backup = bc.id_backup AND bm.id_backup = bac.id_backup AND bm.id_account = bac.id_account AND bm.id_backup = $idBackup AND bm.id_category >= 10000) AS CC");
 
-        /*$select = $this -> m -> mostrar("bm.id_backup = bc.id_backup AND bm.id_backup = $idBackup", "bm.*, bc.symbol", "backup_movements bm, backup_currencies bc");
+        $select = $this -> m -> mostrar("bm.id_backup = bc.id_backup AND bm.id_backup = $idBackup", "bm.*, bc.symbol", "backup_movements bm, backup_currencies bc");
         $select = $this -> m -> mostrar("bm.id_backup = bc.id_backup AND bm.id_backup = bac.id_backup AND bm.id_account = bac.id_account AND bm.id_backup = bcat.id_backup AND (bm.id_category = bcat.id_category OR bm.id_category >= 10000) AND bm.id_backup = $idBackup",
             "bm.*, bc.symbol, bac.name as account, bcat.name as category",
             "backup_movements bm, backup_currencies bc, backup_accounts bac, backup_categories bcat");*/
 
-        $arreglo = array();
+        $arreglo["consultaSQL"] = $this -> consultaSQL($this -> select, $this -> table, $this -> where);
+        $select = $this -> m -> mostrar($this -> where, $this -> select, $this -> table);
+
         if ($select) {
             $arreglo["error"] = false;
             $arreglo["movements"] = $select;
-            $arreglo["titulo"] = "¡ MOVEMENTS ENCONTRADOS !";
-            $arreglo["msj"] = "Se encontraron movements del respaldo solicitado.";
+            $arreglo["titulo"] = ($isQuery) ? "¡ MOVIMIENTOS ENCONTRADOS !": "¡ MOVIMIENTO ENCONTRADO !";
+            $arreglo["msj"] = (($isQuery) ? "Se encontraron movimientos con " : "Se encontro movimiento") . $this -> keyValueArray($this -> pk_Movement);
+            if ($isQuery && $this -> pagina == 0) {
+                $this -> ctrlAccount = new ControlAccount($this -> pk_Movement["id_backup"]);
+                $arreglo["accountsBackup"] = $this -> ctrlAccount -> obtAccountsBackup(false);
+            }
         } else {
             $arreglo["error"] = true;
-            $arreglo["titulo"] = "¡ MOVEMENTS NO ENCONTRADOS !";
-            $arreglo["msj"] = "No se encontraron movements del respaldo solicitado.";
+            $arreglo["titulo"] = ($isQuery) ? "¡ MOVIMIENTOS NO ENCONTRADOS !": "¡ MOVIMIENTO NO ENCONTRADO !";
+            $arreglo["msj"] = (($isQuery) ? "NO se encontraron movimientos con " : "NO se encontro movimiento") . $this -> keyValueArray($this -> pk_Movement);
         }
         return $arreglo;
     }
